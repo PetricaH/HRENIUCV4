ed3da48629ae1e06f42c4387b6a8fef95f209996
PetricaH <petricahphp@gmail.com>
2024-09-06 22:40:38 +0300

    update

--- index.php
+++ index.php
@@ -14,13 +14,14 @@
             <?php include( ROOT_PATH . '/includes/banner.php') ?>
 
             <h1 class="hero-section-title">HI, I'M HRENIUC.</h1>
+            <h1 class="hero-section-title-outline">HI, I'M HRENIUC.</h1>
             <div class="hero-section-copy-container">
-            <img src="static/images/me.png" alt="me" id="me-img">
+            <img src="static/images/meV2.png" alt="me" id="me-img">
                 <div class="hero-section-copy-container-groups">
                     <div class="hero-section-copy-container-text-group">
                         <p>Master's student in Marketing, Web Developer, Digital Marketer, Digital Artist</p>
                         <p>Jack of all trades, master of all of them!</p>
-                        <p>Until something new appears and I have to learn it. :D</p>
+                        <p>Until something new appears and I have to lear it. :D</p>
                     </div>
                     <div class="hero-section-copy-container-buttons-group">
                         <button class="contact-me-btn">Contact Me</button>
