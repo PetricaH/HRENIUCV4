9ec70c3063849f7022dccaa5680d39f410bf062b
PetricaH <petricahphp@gmail.com>
2024-09-13 23:31:04 +0300

    update

--- includes/head_section.php
+++ includes/head_section.php
@@ -7,3 +7,4 @@
         <link rel="stylesheet" href="static/css/public_styling.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="./static/js/public_script.js"></script>
