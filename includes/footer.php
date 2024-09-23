7bb2a38da930fcb16e7128e11683afe5163118c0
PetricaH <petricahphp@gmail.com>
2024-09-23 21:39:22 +0300

    updatE

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,26 @@
 <div class="footer">
-                        <p>MyViewers &copy; <?php echo date('Y'); ?></p>
+        <div class="footer-group">
+                <ul>
+                        <li>Home</li>
+                        <li>Concat</li>
+                        <li>Web Development</li>
+                        <li>Digital Art</li>
+                </ul>
+                <ul>
+                        <li>Privacy Policy</li>
+                        <li>Cookies</li>
+                </ul>
+                <div class="newsletter-subscribe">
+                        <p>Do you want to be up to date with the latest trends in any of the domains I'm active in?</p>
+                        <input type="text" id="email" name="email" required>
+                        <button type="submit">Subscribe</button>
                 </div>
-        </div>
+        </div>                   
+
+
+
+ <!-- <p>MyViewers &copy; <?php echo date('Y'); ?></p> -->
+</div>
         <!-- // container -->
         <script src="./static/js/animations.js"></script>
 </body>
