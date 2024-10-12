b8a74feadac4781ec94d84ccb6a59a1ea766b77f
PetricaH <petricahphp@gmail.com>
2024-10-12 23:05:11 +0300

    update

---
+++ privacy.php
@@ -0,0 +1,7 @@
+hreniucpetrica.ro ("we", "us", or "our") is commited to protecting the privacy of visitors to our website and ensuring that your personal data is handled in a secure and responsible manner. This Privacy Policy explains how we collect, use, and protect your personal data in accordance with applicable laws, including Regulations (EU) 2016/679 (General Data Protection Regulation - GDPR) and Romanian national laws.
+
+1. Data Controller
+
+The data controller for the processing of personal data through teh website hreniucpetrica.ro is:
+    
+    Hreniuc Petrica
