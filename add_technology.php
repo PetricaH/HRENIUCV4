4a3de6717625dcdeaa035c993141a203159b63ec
PetricaH <petricahphp@gmail.com>
2024-10-26 22:29:40 +0300

    updatE

--- add_technology.php
+++ add_technology.php
@@ -1,3 +1,6 @@
+<?php include('../config.php'); ?>
+<?php include(ROOT_PATH . '/admin/includes/admin_functions.php'); ?>
+<?php include(ROOT_PATH . '/admin/includes/head_section.php'); ?>
 <?php
 require_once('../config.php');
 
@@ -22,3 +25,26 @@ if (isset($_POST['tech_name']) && isset($_FILES['tech_logo'])) {
     }
 }
 ?>
+
+<title>Admin | Add Technology</title>
+</head>
+<body>
+    <?php include(ROOT_PATH . '/admin/includes/navbar.php'); ?>
+    <div class="container content">
+        <?php include(ROOT_PATH . '/admin/includes/menu.php'); ?>
+        
+        <h2>Add New Technology</h2>
+        <form action="add_technology.php" method="POST" enctype="multipart/form-data">
+            <label for="tech_name">Technology Name:</label>
+            <input type="text" name="tech_name" id="tech_name" required>
+            <br>
+
+            <label for="tech_logo">Upload Technology Logo:</label>
+            <input type="file" name="tech_logo" id="tech_logo" required>
+            <br>
+
+            <button type="submit" name="add_technology">Add Technology</button>
+        </form>
+    </div>
+</body>
+</html>
